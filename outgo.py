9b8a4547680eaa5f8514717e16594d07a38def80
mateobosco <mateo.bosco@gmail.com>
2015-01-16 17:37:04 -0300

    ver la funcion de filtro para los tags

--- outgo.py
+++ outgo.py
@@ -1,6 +1,7 @@
 import pymongo
 import datetime
 import sys
+import re
 
 
 class Outgo(object):
@@ -58,11 +59,17 @@ class Program(object):
 		if "new" in self.argv:
 			parameters = self.getNewOutgoParameters()
 			self.addOutgoFromParameters(parameters)
-		
+
 		if "query" in self.argv:
 			#OBTENER LOS PARAMETROS DESDE ARGV
 			o = Outgo()
 			self.base.findOutgo(o)
+
+
+	def getTagsFromString(self,string):
+		tags = re.split(",| ", string)
+		filter(lambda a: a not in [""," ",","], tags)
+		return tags
 
 
 	def getNewOutgoParametersFromArguments(self):
@@ -74,30 +81,33 @@ class Program(object):
 			parameters["cost"] = self.argv[index_new_outgo_cost]
 			index_new_outgo_tags = self.argv.index("-t") + 1
 			string_tags = self.argv[index_new_outgo_tags]
-			parameters["tags"] = string_tags.split(",")
+			parameters["tags"] = self.getTagsFromString(string_tags)
 			return parameters
 		else:
 			print "There are some parameters missing."
+
 
 	def getNewOutgoParametersFromInput(self):
 		parameters={}
 		parameters["desc"] = raw_input("Description: ")
 		parameters["cost"] = input("Cost: ")
-		string_tags = raw_input("Tags (separated by comma): ")
-		parameters["tags"] = string_tags.split(",")
+		string_tags = raw_input("Tags: ")
+		parameters["tags"] = self.getTagsFromString(string_tags)
 		return parameters
+
 
 	def addOutgoFromParameters(self,parameters):
 		o = Outgo(parameters["desc"], parameters["cost"], datetime.datetime.utcnow(), parameters["tags"])
 		print str(o)
 		self.base.addOutgo(o)
 
+
 	def startInteraction(self):
 		e = input("Options: \n 1 - Add a new outgo \n 2 - Query \n")
 		if e == 1:
 			parameters = self.getNewOutgoParametersFromInput()
 			self.addOutgoFromParameters(parameters)
-		if e == 2:
+		elif e == 2:
 			#DO QUERY
 			o = Outgo()
 			self.base.findOutgo(o)
@@ -107,20 +117,14 @@ class Program(object):
 
 
 def main():
-	print "arranca"
 	program = Program()
-	print sys.argv
 
 	if len(sys.argv) > 1:
 		program.parameterDisclaimerFromInput()
 		new_outgo_parameters = program.getNewOutgoParameters()
-		print new_outgo_parameters
 
 	else:
 		program.startInteraction()
-
-
-	print "termina"
 
 
 main()
